2a902db6957eee1e8399f62e9ca7c6bafbccc6f9
Avijit Mandal <46240269+avijit1999@users.noreply.github.com>
2021-07-29 17:32:04 +0530

    Create Leetcode121.java

---
+++ Array/Leetcode121.java
@@ -0,0 +1,30 @@
+//Best Time to Buy and Sell Stock
+import java.util.*;
+public class Leeetcode121{
+
+     public static void main(String []args){
+         Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] A=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            A[i]=sc.nextInt();
+        }
+        System.out.println(Solution.maxProfit(A));
+        
+     }
+}
+
+public class Solution {
+    public int maxProfit(int prices[]) {
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                int profit = prices[j] - prices[i];
+                if (profit > maxprofit)
+                    maxprofit = profit;
+            }
+        }
+        return maxprofit;
+    }
+}
